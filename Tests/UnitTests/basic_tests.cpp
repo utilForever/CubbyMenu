eec0fae5efb2e863f079c4ccd4360331e6926da4
Hyung-Suk Lho <nonaddu@gmail.com>
2020-12-23 13:25:03 +0900

    * Added unit tests - Sub menu - Arguments

--- Tests/UnitTests/basic_tests.cpp
+++ Tests/UnitTests/basic_tests.cpp
@@ -85,4 +85,88 @@ TEST_CASE("CubbyMenu - basic tests")
 
         REQUIRE(test_output.str() == "0. Option 0\n1. Option 1\n2. Option 2\n>> Option 1 is called!\n");
     }
+
+    SUBCASE("Sub Menu")
+    {
+        auto f{ [](auto const& e) {
+            return [e](std::vector<std::string> const&) { std::cout << e << std::endl; };
+        } };
+
+        REQUIRE_NOTHROW(
+            CubbyMenu::Menu{ "MyMenu" }
+                .add_header_ex("Main Menu")
+                .add_item_ex("Option 0", f("Option 0 is called."))
+                .add_item_ex("Option 1", f("Option 1 is called."))
+                .add_item_ex(
+                    CubbyMenu::Menu{ "Sub Menu" }
+                        .add_header_ex("Sub Menu")
+                        .add_item_ex("Option 2.0", f("Option 2.0 is called."))
+                        .add_item_ex(
+                            CubbyMenu::Menu{ "Sub Sub Menu" }
+                                .add_header_ex("Sub Sub Menu")
+                                .add_item_ex("Option 2.1.0",
+                                             f("Option 2.1.0 is called."))
+                                .add_item_ex("Option 2.1.1",
+                                             f("Option 2.1.1 is called."))
+                                .add_item_ex("Option 2.1.2",
+                                             f("Option 2.1.2 is called."))
+                                .add_item_ex("Option 2.1.3",
+                                             f("Option 2.1.3 is called."))
+                                .add_item_ex("Option 2.1.4",
+                                             f("Option 2.1.4 is called.")))
+                        .add_item_ex("Option 2.2", f("Option 2.2 is called."))
+                        .add_item_ex("Option 2.3", f("Option 2.3 is called.")))
+                .add_item_ex("Option 3", f("Option 3 is called."))
+                .print());
+    }
+
+    SUBCASE("Arguments")
+    {
+        auto f{ [](auto fo) {
+            return [fo](std::vector<std::string> const& args) {
+                if (std::size(args) < 3)
+                {
+                    std::cout << "Number of arguments must be at least 3\n";
+
+                    return;
+                }
+
+                try
+                {
+                    std::invoke(fo, std::stoi(args[1]), std::stoi(args[2]));
+                }
+                catch (std::exception const& e)
+                {
+                    std::cout << e.what() << std::endl;
+                }
+            };
+        } };
+
+        REQUIRE_NOTHROW(
+            CubbyMenu::Menu{ "MyMenu" }
+                .add_header_ex("Main Menu")
+                .add_item_ex("Arguments Test",
+                             [](std::vector<std::string> const& args) {
+                                 for (auto i{ 0u }; i < std::size(args); ++i)
+                                     std::cout << "arg " << i << ": " << args[i]
+                                               << std::endl;
+                             })
+                .add_item_ex("Add(int + int)", f([](auto a, auto b) {
+                                 std::cout << a << " + " << b << " = " << a + b
+                                           << '\n';
+                             }))
+                .add_item_ex("Sub(int - int)", f([](auto a, auto b) {
+                                 std::cout << a << " - " << b << " = " << a - b
+                                           << '\n';
+                             }))
+                .add_item_ex("Mul(int * int)", f([](auto a, auto b) {
+                                 std::cout << a << " * " << b << " = " << a * b
+                                           << '\n';
+                             }))
+                .add_item_ex("Div(int / int)", f([](auto a, auto b) {
+                                 std::cout << a << " / " << b << " = " << a / b
+                                           << '\n';
+                             }))
+                .print());
+    }
 }
